ed3cc4115e96d839fe5ea4aef0fb4d43b79d7426
NISARGA SHASHIDHAR <nisarga849@gmail.com>
2021-12-16 22:27:52 +0000

    Update protractor.conf.js

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,12 +1,10 @@
-// @ts-check
-// Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+// the file Protractor-configuration
+
 
 const { SpecReporter, StacktraceOption } = require('jasmine-spec-reporter');
 
-/**
- * @type { import("protractor").Config }
- */
+
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
